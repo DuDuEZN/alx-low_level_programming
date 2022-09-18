cdb66dc3388632b8f6d20bbe4260e8b2ba375408
DuDuEZN <kadoukpe@live.fr>
2022-09-18 02:19:22 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+*main (entry)
+*return (0)
+*\
+
+int main(void)
+{
+    char test_str[] = "\"Programming is like building a multilingual puzzle";
+    puts(test_str);
+    return(0);
+}
