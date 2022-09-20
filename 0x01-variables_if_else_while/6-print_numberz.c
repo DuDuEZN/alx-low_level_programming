3f4ea5e7ec2207bd7321ab87f945c63433d48e5f
DuDuEZN <kadoukpe@live.fr>
2022-09-19 23:19:46 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int d = 48;
+while (n < 58)
+{
+putchar(d);
+d++;
+}
+putchar('\n');
+return (0);
+}
