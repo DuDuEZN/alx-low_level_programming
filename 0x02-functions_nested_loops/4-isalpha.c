fff69729d7571508506c9f6f0029b1a189d5be2e
DuDuEZN <kadoukpe@live.fr>
2022-09-21 11:32:44 -0700

    3. 4. .....

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,17 +2,14 @@
 
 /**
  * _isalpha - check for alphabetic
+ * @c: the character to be checked
  *
- * Return: 1 if c is uppercas or lowercase
+ * Return: 1 if c is letter lowercase or uppercase, 0 otherwise
  */
 int _isalpha(int c)
 {
 if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-{
 return (1);
-}
 else
-{
 return (0);
-}
 }
