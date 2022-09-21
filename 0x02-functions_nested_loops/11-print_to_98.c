6f0a3d48f9cf49ac4090b70344ee0db2467fcc20
DuDuEZN <kadoukpe@live.fr>
2022-09-21 16:41:43 -0700

    11. 98 Bateery street

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include \"main.h\"
+#include <stdio.h>
+
+/**
+ * print_to_98 - print all number from n to 98
+ * @n: starting number
+ *
+ * Return: 0 Always (Success)
+ */
+int print_to_98(int n)
+{
+int f;
+while (n <= 98(
+{
+f = n + 1
+printf("%d, ", f);
+n++;
+}
+}
