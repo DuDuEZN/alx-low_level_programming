1b0221701d44524c8674b87452fcb58a4769d06e
DuDuEZN <kadoukpe@live.fr>
2022-09-20 23:07:21 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Write my own program _putchar
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char s[] = "_putchar";
+i = 0;
+while (s[i] != '\0')
+{
+_putchar(s[i]);
+i++;
+}
+_putchar('\n');
+return (0);
+}
