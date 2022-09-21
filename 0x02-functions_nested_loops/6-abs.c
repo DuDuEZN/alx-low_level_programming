71669d05d41f2f16acad6678f75993959b53359e
DuDuEZN <kadoukpe@live.fr>
2022-09-21 12:47:27 -0700

    6. .....

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - compute the absolute value of an integer
+ *
+ *
+ *
+ */
+
+
+int _abs(int t)
+{
+if (t < 0)
+{
+return (-t);
+}
+else
+{
+return (t);
+}
+}
