b86ef445892d2be6dd23d7a7bbe002389e4584e8
DuDuEZN <kadoukpe@live.fr>
2022-09-21 18:01:29 -0700

    12. .......

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,9 +8,9 @@
  */
 void print_times_table(int n)
 {
+int t, i, h;
 if (n <= 15 && n >= 0)
 {
-int t, i, h;
 for (t = 0; t <= n; t++)
 {
 _putchar('0');
@@ -22,17 +22,15 @@ _putchar(' ');
 if (h <= 9)
 {
 _putchar(' ');
-_putchar(h + '0');
 }
 if ( h <= 99)
 {
 _putchar(' ');
-_putchar(h + '0');
 }
 if (h >= 100)
 {
 _putchar((h / 100) + '0');
-_putchar(((h / 10) % 10) + '0');
+_putchar(((h / 10)) % 10 + '0');
 }
 else if (h <= 99 && h>= 10)
 {
