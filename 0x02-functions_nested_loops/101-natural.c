91b8a313fb4f916b5cb726e40f211bd1b76be613
DuDuEZN <kadoukpe@live.fr>
2022-09-21 18:18:08 -0700

    13. ....

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include "main.h"
+include <stdio.h>
+/**
+ * main - list all natural number 
+ * 
+ * Return: Always 0
+ */
+int main(void)
+{
+int n = 0, h;
+if (n <= 1024)
+{
+while ((n % 3) == 0 || (n % 5) == 0)
+{
+h = h + n
+n++
+}
+}
+print("%d\n", h);
+}
