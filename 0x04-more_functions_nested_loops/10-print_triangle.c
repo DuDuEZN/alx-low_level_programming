87be8895a4403c0d9e757486e8793effdb8e895c
DuDuEZN <kadoukpe@live.fr>
2022-09-24 09:24:58 -0700

    10. Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+
+#include "main.h"
+/**
+ * print_triangle - a function that prints a triangle
+ * @size: number of duplication
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+int n, t;
+
+for (n = 0; n < size; n++)
+{
+for (t = 1; t < (size -1); t++)
+{
+_putchar(' ');
+}
+for (t--; t < size; t++)
+{
+_putchar(35);
+}
+if (n < (size -1))
+{
+_putchar('\n');
+}
+}
+_putchar('\n');
+}
