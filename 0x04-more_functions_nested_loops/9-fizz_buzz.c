98e572df4a87a5c4cd40ecf8bc778d47de45b9e2
DuDuEZN <kadoukpe@live.fr>
2022-09-24 08:47:51 -0700

    9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,17 +13,17 @@ int n = 1;
 
 while (n < 101)
 {
-if ((n % 3) == 0)
+if ((n % 3) == 0 && n % 5 == 0)
+{
+printf("FizzBuzz ");
+}
+else if ((n % 3) == 0)
 {
 printf("Fizz ");
 }
-else if ((n % 5) == 0 && n != 100)
+else if (n % 5 == 0 && n != 100)
 {
 printf("Buzz ");
-}
-else if (n % 5 == 0 && n % 3 == 0)
-{
-printf("FizzBuzz ");
 }
 else if (n == 100)
 {
