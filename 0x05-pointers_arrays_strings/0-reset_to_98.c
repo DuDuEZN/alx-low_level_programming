8925c370364687e7464af4e6aeed4298364976c7
DuDuEZN <kadoukpe@live.fr>
2022-09-25 14:54:24 -0700

    98 Battery St.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 
 /**
  * reset_to_98 -  a function that takes a pointer to an int as parameter
- * @ *n: value of pointer to be printed
+ * @n: value of pointer to be printed
  */
 
 void reset_to_98(int *n)
