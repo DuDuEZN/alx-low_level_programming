83a1289aed890a89f3fe222b940a1f8b7f8e3ee8
DuDuEZN <kadoukpe@live.fr>
2022-09-26 06:32:00 -0700

    3. ...

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+
+#include "main.h"
+
+/**
+ * _puts - a function that prints a string, followed by a new line
+ * @str: string to be checkes
+ * Return: result
+ */
+
+void _puts(char *str)
+{
+int n = 0;
+while (n >= 0)
+{
+if (str[n] == '\0')
+{
+_putchar('\n');
+}
+_putchar(str[n];
+n++;
+}
+}
