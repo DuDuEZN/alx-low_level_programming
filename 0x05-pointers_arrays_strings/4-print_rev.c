4c5823df2d1d38367e417fb7a8ad7ebd9df8e25a
DuDuEZN <kadoukpe@live.fr>
2022-09-26 06:48:06 -0700

    4. ...

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,11 +3,9 @@
 #include "main.h"
 
 /**
- * print_rev - a function that prints a string, in reverse, followed by a new line.
+ * print_rev - a function that prints a string, in reverse
  * @s: string to be checked
  */
-
-
 void print_rev(char *s)
 {
 int t = 0;
